0a88e198760d54cb71e01beb50cda2704ddcf483
gsakun <gsakun7@gmail.com>
2024-02-27 17:35:20 +0800

    fix worker register overwrite old worker labels

--- internal/controller/api_workers.go
+++ internal/controller/api_workers.go
@@ -65,7 +65,9 @@ func (controller *Controller) createWorker(ctx *gin.Context) responder.Responder
 					"consider upgrading at https://tart.run/licensing/", controller.maxWorkersPerLicense))
 			}
 		}
-
+		if dbWorker.Labels != nil {
+			worker.Labels = dbWorker.Labels
+		}
 		if err := txn.SetWorker(worker); err != nil {
 			return responder.Error(err)
 		}
